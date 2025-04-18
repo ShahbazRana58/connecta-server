9736ecdaac1680fef19532bc88b648d606421ddf
ShahbazRana58 <mshahbazrana1920@gmail.com>
2025-04-18 10:56:37 +0500

    heroku deployment 1

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+var http = require("http");
+const app = express();
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+
+//middleware
+app.use(express.json());
+var clients = {};
+
+
+io.on("connection",(socket)=>{
+    console.log("Connected");
+    console.log(socket.id, "has joined");
+    socket.on("signin", (id)=> {
+        console.log(id);
+        clients[id]=socket;
+        console.log(clients);
+    });
+    socket.on("message",(msg)=>{
+        console.log(msg);
+        targetID = msg.targetID;
+        if(clients[targetID]){
+            clients[targetID].emit("message",msg);
+            console.log(`Message sent to ${targetID}`);
+        }
+        else{
+            console.log(`Message not sent to ${targetID}`);
+        }
+    });
+});
+
+app.route("/check").get((req,res)=>{
+    return res.json("Your App is working Fine.")
+});
+
+server.listen(port, "0.0.0.0", ()=>{
+    console.log("Server Started");
+});
